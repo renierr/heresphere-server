cd27351fa6308ef0a63020c4dbb70bcfd2472d55
renierr <github@renier.de>
2024-12-13 13:19:35 +0100

    check only part of name start

--- globals.py
+++ globals.py
@@ -6,6 +6,6 @@ url_counter = 1
 def find_url_info(filename):
     for idnr, url_info in url_map.items():
         filename_check = os.path.splitext(filename.rstrip('.part'))[0]
-        if url_info['filename'] == filename_check:
+        if filename_check.startswith(url_info['filename']):
             return idnr, url_info
     return None, None
