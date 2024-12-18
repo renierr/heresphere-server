9d55243b68f9936db1d1d048beb454977e3a72b3
Renier <github@renier.de>
2024-12-18 01:12:23 +0100

    just take 8 seconds from middle - much faster then mapping

--- thumbnail.py
+++ thumbnail.py
@@ -117,20 +117,27 @@ def generate_thumbnail(video_path, thumbnail_path):
             #subprocess.run([
             #    'ffmpeg', '-ss', str(midpoint), '-y', '-i', video_path, '-vf', 'thumbnail,scale=w=380:h=240:force_original_aspect_ratio=decrease', '-frames:v', '5', os.path.splitext(thumbnail_path)[0] + '.webm'
             #], check=True, stdout=stdout, stderr=stdout)
-            filter_complex = ""
-            for i, point in enumerate(points):
-                filter_complex += f"[0:v]trim=start={point}:duration={segment_duration},setpts=PTS-STARTPTS,scale=w=380:h=-1[v{i}];"
-                filter_complex += f"[0:a]atrim=start={point}:duration={segment_duration},asetpts=PTS-STARTPTS[a{i}];"
 
-            filter_complex += "".join([f"[v{i}][a{i}]" for i in range(len(points))])
-            filter_complex += f"concat=n={len(points)}:v=1:a=1[v][a]"
-
+            # time period to take thumbnail
             command = [
-                'ffmpeg', '-y', '-i', video_path, '-filter_complex', filter_complex,
-                '-map', '[v]', '-map', '[a]', '-c:v', 'libvpx', '-deadline', 'realtime', '-cpu-used', '16', '-crf', '8', '-b:v', '256k', '-c:a', 'libvorbis', os.path.splitext(thumbnail_path)[0] + '.webm'
+                'ffmpeg', '-ss', str(midpoint), '-t', '8', '-y', '-i', video_path, '-vf', 'scale=380:-1', '-c:v', 'libvpx', '-deadline', 'realtime', '-cpu-used', '16', '-crf', '8', '-b:v', '256k', '-c:a', 'libvorbis', os.path.splitext(thumbnail_path)[0] + '.webm'
             ]
-            logger.debug(f"Running command: {' '.join(command)}")
 
+
+            # complex filter to generate webm thumbnail
+            # filter_complex = ""
+            # for i, point in enumerate(points):
+            #     filter_complex += f"[0:v]trim=start={point}:duration={segment_duration},setpts=PTS-STARTPTS,scale=w=380:h=-1[v{i}];"
+            #     filter_complex += f"[0:a]atrim=start={point}:duration={segment_duration},asetpts=PTS-STARTPTS[a{i}];"
+            #
+            # filter_complex += "".join([f"[v{i}][a{i}]" for i in range(len(points))])
+            # filter_complex += f"concat=n={len(points)}:v=1:a=1[v][a]"
+            # command = [
+            #     'ffmpeg', '-y', '-i', video_path, '-filter_complex', filter_complex,
+            #     '-map', '[v]', '-map', '[a]', '-c:v', 'libvpx', '-deadline', 'realtime', '-cpu-used', '16', '-crf', '8', '-b:v', '256k', '-c:a', 'libvorbis', os.path.splitext(thumbnail_path)[0] + '.webm'
+            # ]
+
+            logger.debug(f"Running command: {' '.join(command)}")
             subprocess.run(command, check=True, stdout=stdout, stderr=stdout)
 
         logger.debug(f"Generating thumbnail for {video_path} finished.")
