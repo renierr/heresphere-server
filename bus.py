a5ee666b42e217031b8d0bba3ba96de4811eaf4e
Renier <github@renier.de>
2024-12-19 01:06:05 +0100

    code cleanup

--- bus.py
+++ bus.py
@@ -1,26 +1,10 @@
 from loguru import logger
-import queue
 
-class AutoRemovingQueue(queue.Queue):
-    def __init__(self, maxsize=100):
-        super().__init__(maxsize)
-
-    def put(self, item, block=True, timeout=None):
-        while self.full():
-            try:
-                self.get_nowait()
-            except queue.Empty:
-                break
-        super().put(item, block, timeout)
-
-event_bus = AutoRemovingQueue(maxsize=100)
+clients = []
 
 def push_text_to_client(txt):
     logger.debug(f"{txt}")
     broadcast_message(txt)
-
-
-clients = []
 
 def broadcast_message(message):
     for client_queue, stop_event in clients:
