34ddc86b57806ffef1a86e8503a8256b1bdad3a7
Renier <github@renier.de>
2024-12-22 17:27:52 +0100

    fix None data

--- globals.py
+++ globals.py
@@ -34,7 +34,7 @@ def find_url_id(url):
 def find_url_info(filename):
     for idnr, url_info in url_map.items():
         filename_check = os.path.splitext(filename.rstrip('.part'))[0]
-        if filename_check.startswith(url_info['filename']):
+        if filename and url_info['filename'] and filename_check.startswith(url_info['filename']):
             return idnr, url_info
     return None, None
 
