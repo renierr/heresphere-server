6e08f5594eb8c66a0a93cac84d81e72d2e1f7f4e
Renier <github@renier.de>
2025-03-05 00:32:55 +0100

    try to switch to vue3

--- static/js/components/server-info.js
+++ static/js/components/server-info.js
@@ -9,4 +9,32 @@ export const ServerInfo = {
             serverResult: '',
         };
     },
+    methods: {
+        openAndHandleSSEConnection(call_func) {
+            const eventSource = new EventSource('/sse');
+            window.addEventListener('beforeunload', () => {
+                eventSource.close();
+                console.log('EventSource connection closed');
+            });
+
+            const serverOutput = [];
+            eventSource.onmessage = event => {
+                // ignore Heartbeat message
+                if (event.data.includes('Heartbeat '))  return;
+
+                serverOutput.push(new Date().toLocaleTimeString() + ': ' + event.data);
+                if (serverOutput.length > 100) {
+                    serverOutput.shift();
+                }
+                this.serverOutput = serverOutput.slice().reverse().join('\n');
+                if (call_func)  {
+                    call_func(event);
+                }
+            };
+            return eventSource;
+        },
+    },
+    mounted() {
+        this.openAndHandleSSEConnection();
+    }
 }
